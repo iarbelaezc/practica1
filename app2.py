27b7eea1ff2650ae18028ec913d8f04bd68c5f7f
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-03-12 13:57:01 -0500

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+from PIL import Image 
+
+st.title(" Aplicación de alertas tempranas")
+
+st.header (" En este espacio se podrá obtener información sobre presipitación, calidad del aire y movimientos en masa")
+
+image= Image.open
